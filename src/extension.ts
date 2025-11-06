2e48b80ca65bef3e99c0a16850967382ae6e3aaa
Franz Srambical <franz.srambical@gmail.com>
2025-11-06 11:15:11 +0100

    feat: execute hardcoded script using tabbing in both editor and terminal

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "crowd-pilot" is now active!');
 
+	// Configure terminal to allow tab keybinding to work
+	// This makes the command skip the shell so VS Code can intercept tab in terminals
+	const config = vscode.workspace.getConfiguration('terminal.integrated');
+	const commandsToSkipShell = config.get<string[]>('commandsToSkipShell', []);
+	if (!commandsToSkipShell.includes('crowd-pilot.testRun')) {
+		commandsToSkipShell.push('crowd-pilot.testRun');
+		config.update('commandsToSkipShell', commandsToSkipShell, vscode.ConfigurationTarget.Global);
+	}
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -22,15 +31,15 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 	const testRun = vscode.commands.registerCommand('crowd-pilot.testRun', async () => {
 		const editor = vscode.window.activeTextEditor;
-		const doc = editor.document;
+		const doc = editor!.document;
 		const term = vscode.window.terminals[0] ?? vscode.window.createTerminal('Test');
 		const git = vscode.extensions.getExtension('vscode.git')?.exports?.getAPI(1);
 		const repo = git?.repositories?.[0];
 	
 		// Emit a few actions:
 		await vscode.window.showTextDocument(doc);
-		editor.selections = [new vscode.Selection(0, 0, 0, 0)];
-		await editor.edit(e => e.insert(new vscode.Position(0, 0), 'hello world\n'));
+		editor!.selections = [new vscode.Selection(0, 0, 0, 0)];
+		await editor!.edit(e => e.insert(new vscode.Position(0, 0), 'hello world\n'));
 		term.show();
 		term.sendText('echo VSCode test');
 		//await repo?.pull();
